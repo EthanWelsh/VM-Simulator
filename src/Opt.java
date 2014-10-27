fb6434ef0bd8a44a713e5b48b781d2b4caffe6c3
welshej <welshej@gmail.com>
2014-10-27 12:44:29 -0400

    construct data structure for opt

--- src/Opt.java
+++ src/Opt.java
@@ -1,6 +1,46 @@
-/**
- * Created by welshej on 10/26/14.
- */
+import java.io.*;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Queue;
+
 public class OPT
 {
+	HashMap<Integer, ArrayList<Integer>> priorityMap;
+
+	public OPT(String fname) throws IOException
+	{
+
+		File file = new File(fname);
+		BufferedReader reader = new BufferedReader(new FileReader(file));
+
+		String line;
+		int indexInFile = 0;
+
+		while ((line = reader.readLine()) != null)
+		{
+			String [] splitter = line.split(" ");
+			int pageNumber = Integer.parseInt(splitter[0].substring(0,2), 16);
+
+			if(priorityMap.containsKey(pageNumber))
+			{
+				priorityMap.get(pageNumber).add(indexInFile);
+			}
+			else
+			{
+				ArrayList<Integer> q = new ArrayList<Integer>();
+				q.add(indexInFile);
+				priorityMap.put(pageNumber, q);
+			}
+			indexInFile++;
+		}
+
+
+
+	}
+
+
+
+
+
 }
